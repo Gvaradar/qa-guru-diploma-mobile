e3a6d154af39c6dfa979d2eb53445a2b867a5560
Djon <lioni767@yandex.ru>
2026-06-03 03:18:11 +0300

    add PageObject and remove with allure.step from tests

---
+++ pages/main_page.py
@@ -0,0 +1,95 @@
+from appium.webdriver.common.appiumby import AppiumBy
+import allure
+
+
+class MainPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    SEARCH_BUTTON = (AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')
+    SEARCH_INPUT = (AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')
+    RESULT_TITLE = (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
+    MENU_BUTTON = (AppiumBy.ACCESSIBILITY_ID, 'Open navigation drawer')
+    SETTINGS_OPTION = (AppiumBy.XPATH, '//*[@text="Settings"]')
+    SETTINGS_TITLE = (AppiumBy.XPATH, '//*[@text="Settings"]')
+    MORE_OPTIONS = (AppiumBy.ACCESSIBILITY_ID, 'More options')
+    ADD_TO_BOOKMARKS = (AppiumBy.XPATH, '//*[@text="Add to bookmarks"]')
+    CREATE_BOOKMARK_BUTTON = (AppiumBy.ID, 'org.wikipedia.alpha:id/create_bookmark_button')
+    TEXT_INPUT = (AppiumBy.ID, 'org.wikipedia.alpha:id/text_input')
+    OK_BUTTON = (AppiumBy.ID, 'android:id/button1')
+    SNACKBAR_ACTION = (AppiumBy.ID, 'org.wikipedia.alpha:id/snackbar_action')
+    MY_LISTS_OPTION = (AppiumBy.XPATH, '//*[@text="My lists"]')
+    FAVORITES_FOLDER = (AppiumBy.XPATH, '//*[@text="My Favorites"]')
+
+    @allure.step("Нажать на поле поиска")
+    def click_search(self):
+        self.driver.find_element(*self.SEARCH_BUTTON).click()
+        return self
+
+    @allure.step("Ввести текст для поиска: {text}")
+    def enter_search_text(self, text):
+        self.driver.find_element(*self.SEARCH_INPUT).send_keys(text)
+        return self
+
+    @allure.step("Проверить, что результаты поиска отображаются")
+    def assert_search_results_exist(self):
+        results = self.driver.find_elements(*self.RESULT_TITLE)
+        assert len(results) > 0
+        return self
+
+    @allure.step("Открыть меню")
+    def open_menu(self):
+        self.driver.find_element(*self.MENU_BUTTON).click()
+        return self
+
+    @allure.step("Выбрать настройки")
+    def open_settings(self):
+        self.driver.find_element(*self.SETTINGS_OPTION).click()
+        return self
+
+    @allure.step("Проверить, что открылась страница настроек")
+    def assert_settings_opened(self):
+        assert self.driver.find_element(*self.SETTINGS_TITLE).is_displayed()
+        return self
+
+    @allure.step("Найти статью: {text}")
+    def search_article(self, text):
+        self.click_search()
+        self.enter_search_text(text)
+        self.driver.find_element(*self.RESULT_TITLE).click()
+        return self
+
+    @allure.step("Открыть меню статьи")
+    def open_article_menu(self):
+        self.driver.find_element(*self.MORE_OPTIONS).click()
+        return self
+
+    @allure.step("Выбрать 'Add to bookmarks'")
+    def add_to_bookmarks(self):
+        self.driver.find_element(*self.ADD_TO_BOOKMARKS).click()
+        return self
+
+    @allure.step("Создать новую папку для закладок: {folder_name}")
+    def create_bookmark_folder(self, folder_name):
+        self.driver.find_element(*self.CREATE_BOOKMARK_BUTTON).click()
+        self.driver.find_element(*self.TEXT_INPUT).send_keys(folder_name)
+        self.driver.find_element(*self.OK_BUTTON).click()
+        return self
+
+    @allure.step("Подтвердить сохранение")
+    def confirm_save_bookmark(self):
+        self.driver.find_element(*self.SNACKBAR_ACTION).click()
+        return self
+
+    @allure.step("Открыть закладки")
+    def open_bookmarks(self):
+        self.open_menu()
+        self.driver.find_element(*self.MY_LISTS_OPTION).click()
+        self.driver.find_element(*self.FAVORITES_FOLDER).click()
+        return self
+
+    @allure.step("Проверить, что статья '{expected_title}' сохранена")
+    def assert_article_saved(self, expected_title):
+        saved_article = self.driver.find_element(*self.RESULT_TITLE)
+        assert saved_article.text == expected_title
+        return self
--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,10 +1,5 @@
 import allure
-import pytest
-from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from config.config import load_config
+from pages.main_page import MainPage
 
 
 @allure.epic('Mobile тестирование')
@@ -15,56 +10,29 @@ class TestWikipedia:
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.title('Поиск статьи по запросу')
     def test_search_article(self, driver):
-        with allure.step('Нажать на поле поиска'):
-            driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia').click()
-
-        with allure.step('Ввести текст для поиска'):
-            search_input = driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')
-            search_input.send_keys('Python')
-
-        with allure.step('Проверить, что результаты поиска отображаются'):
-            results = driver.find_elements(AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
-            assert len(results) > 0
+        main_page = MainPage(driver)
+        main_page.click_search()
+        main_page.enter_search_text('Python')
+        main_page.assert_search_results_exist()
 
     @allure.story('Навигация')
     @allure.severity(allure.severity_level.NORMAL)
     @allure.title('Открытие настроек')
     def test_open_settings(self, driver):
-        with allure.step('Открыть меню'):
-            driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Open navigation drawer').click()
-
-        with allure.step('Выбрать настройки'):
-            driver.find_element(AppiumBy.XPATH, '//*[@text="Settings"]').click()
-
-        with allure.step('Проверить, что открылась страница настроек'):
-            assert driver.find_element(AppiumBy.XPATH, '//*[@text="Settings"]').is_displayed()
+        main_page = MainPage(driver)
+        main_page.open_menu()
+        main_page.open_settings()
+        main_page.assert_settings_opened()
 
     @allure.story('Сохранение')
     @allure.severity(allure.severity_level.NORMAL)
     @allure.title('Сохранение статьи в закладки')
     def test_save_article_to_bookmarks(self, driver):
-        with allure.step('Найти статью'):
-            driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia').click()
-            driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text').send_keys('Python')
-            driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title').click()
-
-        with allure.step('Открыть меню статьи'):
-            driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'More options').click()
-
-        with allure.step('Выбрать "Add to bookmarks"'):
-            driver.find_element(AppiumBy.XPATH, '//*[@text="Add to bookmarks"]').click()
-
-        with allure.step('Создать новую папку'):
-            driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/create_bookmark_button').click()
-            driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/text_input').send_keys('My Favorites')
-            driver.find_element(AppiumBy.ID, 'android:id/button1').click()
-
-        with allure.step('Подтвердить сохранение'):
-            driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/snackbar_action').click()
-
-        with allure.step('Проверить, что статья сохранена'):
-            driver.find_element(AppiumBy.ACCESSIBILITY_ID, 'Open navigation drawer').click()
-            driver.find_element(AppiumBy.XPATH, '//*[@text="My lists"]').click()
-            driver.find_element(AppiumBy.XPATH, '//*[@text="My Favorites"]').click()
-            saved_article = driver.find_element(AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
-            assert saved_article.text == 'Python'
+        main_page = MainPage(driver)
+        main_page.search_article('Python')
+        main_page.open_article_menu()
+        main_page.add_to_bookmarks()
+        main_page.create_bookmark_folder('My Favorites')
+        main_page.confirm_save_bookmark()
+        main_page.open_bookmarks()
+        main_page.assert_article_saved('Python')
